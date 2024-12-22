e0b4acf1193b9a330e4c5a2dfc1682940a4036a6
stacytaylor <stacy.taylor@gmail.com>
2024-12-22 14:56:31 -0800

    created collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,14 @@
-import { defineCollection, z } from "astro:content";
+import {z, defineCollection} from "astro:content"
 
 const postsCollection = defineCollection({
     schema: z.object({
         author: z.string(),
         date: z.string(),
         image: z.string(),
-       title:z.string() 
-
+        title: z.string()
     })
 })
 
 export const collections = {
-    posts: postsCollection
-}
+    posts: postsCollection,
+};
